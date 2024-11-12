8fe2155072ac7e9e77136c5159cfd764084d5bda
JoIz <113427303+Joiz2022@users.noreply.github.com>
2024-11-12 01:13:10 +0100

    Mejoras UI

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,52 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float initialVelocity = 4f;
-    [SerializeField] private float valocityMultiplier = 1.1f;
+	[SerializeField] public float initialVelocity = 4f;
+	[SerializeField] private float valocityMultiplier = 1.1f;
 
-    private Rigidbody2D ballRb;
-    void Start()
-    {
-        ballRb = GetComponent<Rigidbody2D>();
-        Launch();
-    }
-    private void Launch()
-    {
-        float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
+	private Rigidbody2D ballRb;
+	private bool isPlaying;
+
+
+	void Start()
+	{
+		if (!isPlaying)
+		{
+			initialVelocity = 0.0f;
+		}
+
+		ballRb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		GameManager.Instance.Winner();
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			isPlaying = true;
+			initialVelocity = 4f;
+			Launch();
+			GameManager.Instance.Delete();
+		}
+	}
+	private void Launch()
+	{
+		float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
 		float yVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
-        ballRb.velocity = new Vector2 (xVelocity, yVelocity) * initialVelocity;
+		ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
 
 	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.gameObject.CompareTag("Paddle"))
-        {
-            ballRb.velocity *= valocityMultiplier;
-
+		{
+			ballRb.velocity *= valocityMultiplier;
 		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-        if (collision.gameObject.CompareTag("Goal1"))
-        {
-            // Si toca goal1 ejecutamos funcion de paddle2score
-            GameManager.Instance.Paddle2Score();
-            // Reiniciamos las paletas y bola a su posicion inicial
-            GameManager.Instance.Restart();
-            // Lanzamos la bola
-            Launch();
-        }
-        if (collision.gameObject.CompareTag("Goal2"))
-        {
-            GameManager.Instance.Paddle1Score();
-            GameManager.Instance.Restart();
-            Launch();
-        }
+		if (collision.gameObject.CompareTag("Goal1"))
+		{
+			// Si toca goal1 ejecutamos funcion de paddle2score
+			GameManager.Instance.Paddle2Score();
+			// Reiniciamos las paletas y bola a su posicion inicial
+			GameManager.Instance.Restart();
+			// Lanzamos la bola
+			Launch();
+		}
+		if (collision.gameObject.CompareTag("Goal2"))
+		{
+			GameManager.Instance.Paddle1Score();
+			GameManager.Instance.Restart();
+			Launch();
+		}
 	}
-
-	
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,20 @@ public class GameManager : MonoBehaviour
 {
 	[SerializeField] private TMP_Text paddle1ScoreText;
 	[SerializeField] private TMP_Text paddle2ScoreText;
+	[SerializeField] private TMP_Text winnerText;
+	[SerializeField] private TMP_Text start;
 
 	[SerializeField] private Transform paddle1Transform;  
 	[SerializeField] private Transform paddle2Transform;
 	[SerializeField] private Transform ballTransform;
 
+	[SerializeField] private Ball ballVelocity;
+
 	private int paddle1Score;
 	private int paddle2Score;
+	private string winner1 = "Player 2 Winner";
+	private string winner2 = "Player 1 winner";
+	private bool endGame;
 
 	private static GameManager instance;
 
@@ -28,23 +35,70 @@ public class GameManager : MonoBehaviour
 			return instance;
 		}
 	}
+	void Start()
+	{
+		// Si no se asigna la referencia en el Inspector, la buscamos en el mismo GameObject
+		if (ballVelocity== null)
+		{
+			ballVelocity= FindObjectOfType<Ball>();
+		}
+	}
 
 	public void Paddle1Score()
 	{
-		paddle1Score++;
-		paddle1ScoreText.text = paddle1Score.ToString();
-		Debug.Log(paddle1ScoreText);
+		paddle2Score++;
+		paddle2ScoreText.text = paddle2Score.ToString();
+		Winner();
+		
 	}
 	public void Paddle2Score()
 	{
-		paddle2Score++;
-		paddle2ScoreText.text = paddle2Score.ToString();
+		paddle1Score++;
+		paddle1ScoreText.text = paddle1Score.ToString();
+		Winner();
 	}
 
 	public void Restart()
 	{
 		paddle1Transform.position = new Vector2(paddle1Transform.position.x, 0);
-		paddle2Transform.position = new Vector2(paddle2Transform.position.x,0);
+		paddle2Transform.position = new Vector2(paddle2Transform.position.x, 0);
 		ballTransform.position = new Vector2(0, 0);
+	}
+
+
+	public void Winner()
+	{
+		if (paddle1Score == 5)
+		{
+			winnerText.text = winner1; // Mostramos el texto del ganador
+			EndGame(); // Terminamos el juego
+		}
+		else if (paddle2Score == 5)
+		{
+			winnerText.text = winner2; // Mostramos el texto del ganador
+			EndGame(); // Terminamos el juego
+		}
+		else if (ballVelocity.GetComponent<Rigidbody2D>().velocity.magnitude > 0)
+		{
+			winnerText.text = "";
+		}
+	}
+
+
+	public void EndGame()
+	{
+		paddle1Score = 0;
+		paddle2Score = 0;
+		paddle1ScoreText.text = paddle1Score.ToString();
+		paddle2ScoreText.text = paddle2Score.ToString();
+		ballVelocity.initialVelocity = 0;
+
+		// Borra el texto del ganador al finalizar el juego
+	}
+
+
+	public void Delete()
+	{
+		start.text = "";
 	}
 }
